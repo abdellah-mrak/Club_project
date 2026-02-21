6d65e2fbb1d98fb8761cc61c446ffc8c14ae3869
abdellah-mrak <abdellah.m'rak23@ump.ac.ma>
2026-02-21 13:02:16 +0100

    Update vite.config.js

--- ensiasd-clean/frontend/vite.config.js
+++ ensiasd-clean/frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  // Remplacez "ensiasd-platform" par le nom exact de votre repo GitHub
-  base: '/ensiasd-platform/',
+  base: '/Club_project/',
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') }
